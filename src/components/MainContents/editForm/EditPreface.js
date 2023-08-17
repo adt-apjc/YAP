1591f69b8dfca3879d6739e8bdef38ea1dc86ce1
davyvine <davyvinevaldivia@gmail.com>
2023-08-17 21:42:45 +0800

    #16 refactor preface and validations

--- src/components/MainContents/editForm/EditPreface.js
+++ src/components/MainContents/editForm/EditPreface.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import GlobalContext from "../../contexts/ContextProvider";
 
 import AceEditor from "react-ace";
@@ -8,81 +8,78 @@ import "ace-builds/src-noconflict/theme-github";
 
 import { cloneDeep } from "lodash";
 
-class EditPreface extends React.Component {
-   state = {
-      bodyArr: this.props.initValue ? this.props.initValue.config.bodyArr.join("\n") : "",
-      title: this.props.initValue ? this.props.initValue.config.title : "",
-      stepDesc: this.props.initValue ? this.props.initValue.config.stepDesc : "",
-   };
+const EditPreface = (props) => {
+   const context = useContext(GlobalContext);
+   const [state, setState] = useState({
+      bodyArr: props.initValue ? props.initValue.config.bodyArr.join("\n") : "",
+      title: props.initValue ? props.initValue.config.title : "",
+      stepDesc: props.initValue ? props.initValue.config.stepDesc : "",
+   });
 
-   onEditHandler = () => {
-      let Context = this.context;
-      let currentConfig = cloneDeep(Context.config);
-      if (this.props.initValue) {
+   const onEditHandler = () => {
+      let currentConfig = cloneDeep(context.config);
+      if (props.initValue) {
          // Edit mode
-         currentConfig.preface[this.props.initValue.index].bodyArr = this.state.bodyArr.trim().split("\n");
-         currentConfig.preface[this.props.initValue.index].title = this.state.title;
-         currentConfig.preface[this.props.initValue.index].stepDesc = this.state.stepDesc;
+         currentConfig.preface[props.initValue.index].bodyArr = state.bodyArr.trim().split("\n");
+         currentConfig.preface[props.initValue.index].title = state.title;
+         currentConfig.preface[props.initValue.index].stepDesc = state.stepDesc;
       } else {
          // create new one
          currentConfig.preface.push({
-            bodyArr: this.state.bodyArr.trim().split("\n"),
-            title: this.state.title,
-            stepDesc: this.state.stepDesc,
+            bodyArr: state.bodyArr.trim().split("\n"),
+            title: state.title,
+            stepDesc: state.stepDesc,
          });
       }
-      Context.updateConfig(currentConfig);
-      this.props.onHide();
+      context.updateConfig(currentConfig);
+      props.onHide();
    };
 
-   render() {
-      return (
-         <>
-            <div className="modal-header">
-               <span className="modal-title">Preface Editor</span>
-               <button type="button" className="btn-close" onClick={this.props.onHide}></button>
+   return (
+      <>
+         <div className="modal-header">
+            <span className="modal-title">Preface Editor</span>
+            <button type="button" className="btn-close" onClick={props.onHide}></button>
+         </div>
+         <div className="modal-body">
+            <div className="input-group">
+               <input
+                  className="form-control form-control-sm mb-2 col-6"
+                  placeholder="title"
+                  value={state.title}
+                  onChange={(e) => setState({ ...state, title: e.target.value })}
+               ></input>
+               <input
+                  className="form-control form-control-sm mb-2 col-6"
+                  placeholder="Step title"
+                  value={state.stepDesc}
+                  onChange={(e) => setState({ ...state, stepDesc: e.target.value })}
+               ></input>
             </div>
-            <div className="modal-body">
-               <div className="input-group">
-                  <input
-                     className="form-control form-control-sm mb-2 col-6"
-                     placeholder="title"
-                     value={this.state.title}
-                     onChange={(e) => this.setState({ title: e.target.value })}
-                  ></input>
-                  <input
-                     className="form-control form-control-sm mb-2 col-6"
-                     placeholder="Step title"
-                     value={this.state.stepDesc}
-                     onChange={(e) => this.setState({ stepDesc: e.target.value })}
-                  ></input>
-               </div>
-               <AceEditor
-                  mode="html"
-                  theme="github"
-                  height="500px"
-                  width="100%"
-                  defaultValue={this.state.bodyArr}
-                  onChange={(value) => this.setState({ bodyArr: value })}
-                  placeholder="html content"
-                  name="data"
-                  className="rounded border"
-                  editorProps={{ $blockScrolling: true }}
-                  showPrintMargin={false}
-               />
-            </div>
-            <div className="modal-footer p-1">
-               <button type="button" className="btn btn-sm" onClick={this.props.onHide}>
-                  Close
-               </button>
-               <button type="button" className="btn btn-primary btn-sm" onClick={this.onEditHandler}>
-                  {this.props.initValue ? "Edit" : "Create"}
-               </button>
-            </div>
-         </>
-      );
-   }
-}
-EditPreface.contextType = GlobalContext;
+            <AceEditor
+               mode="html"
+               theme="github"
+               height="500px"
+               width="100%"
+               defaultValue={state.bodyArr}
+               onChange={(value) => setState({ ...state, bodyArr: value })}
+               placeholder="html content"
+               name="data"
+               className="rounded border"
+               editorProps={{ $blockScrolling: true }}
+               showPrintMargin={false}
+            />
+         </div>
+         <div className="modal-footer p-1">
+            <button type="button" className="btn btn-sm" onClick={props.onHide}>
+               Close
+            </button>
+            <button type="button" className="btn btn-primary btn-sm" onClick={onEditHandler}>
+               {props.initValue ? "Edit" : "Create"}
+            </button>
+         </div>
+      </>
+   );
+};
 
 export default EditPreface;
--- src/components/MainContents/editForm/PrefaceDeleteConfirmation.js
+++ src/components/MainContents/editForm/PrefaceDeleteConfirmation.js
@@ -1,38 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import GlobalContext from "../../contexts/ContextProvider";
 import { cloneDeep } from "lodash";
 
-class PrefaceDeleteConfirmation extends React.Component {
-   onDeleteHandler = () => {
-      let Context = this.context;
-      let currentConfig = cloneDeep(Context.config);
-      currentConfig.preface = currentConfig.preface.filter((el, index) => index !== this.props.initValue.index);
-      Context.updateConfig(currentConfig);
-      this.props.onHide();
+const PrefaceDeleteConfirmation = (props) => {
+   let context = useContext(GlobalContext);
+
+   const onDeleteHandler = () => {
+      let currentConfig = cloneDeep(context.config);
+      currentConfig.preface = currentConfig.preface.filter((el, index) => index !== props.initValue.index);
+      context.updateConfig(currentConfig);
+      props.onHide();
    };
 
-   render() {
-      return (
-         <>
-            <div className="modal-header">
-               <span className="modal-title">Confirm Deletion</span>
-               <button type="button" className="btn-close" onClick={this.props.onHide}></button>
-            </div>
-            <div className="modal-body">
-               Title: <span className="text-danger font-weight-light font-italic">{this.props.initValue.config.stepDesc}</span>
-            </div>
-            <div className="modal-footer p-1">
-               <button type="button" className="btn btn-sm" onClick={this.props.onHide}>
-                  Close
-               </button>
-               <button type="button" className="btn btn-danger btn-sm" onClick={this.onDeleteHandler}>
-                  Delete
-               </button>
-            </div>
-         </>
-      );
-   }
-}
-PrefaceDeleteConfirmation.contextType = GlobalContext;
+   return (
+      <>
+         <div className="modal-header">
+            <span className="modal-title">Confirm Deletion</span>
+            <button type="button" className="btn-close" onClick={props.onHide}></button>
+         </div>
+         <div className="modal-body">
+            Title: <span className="text-danger font-weight-light font-italic">{props.initValue.config.stepDesc}</span>
+         </div>
+         <div className="modal-footer p-1">
+            <button type="button" className="btn btn-sm" onClick={props.onHide}>
+               Close
+            </button>
+            <button type="button" className="btn btn-danger btn-sm" onClick={onDeleteHandler}>
+               Delete
+            </button>
+         </div>
+      </>
+   );
+};
 
 export default PrefaceDeleteConfirmation;
