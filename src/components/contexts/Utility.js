a60c288fa0ebcc25dbaa26b6210fc852e60fc318
davyvine <davyvinevaldivia@gmail.com>
2023-09-12 20:45:12 +0800

    #50 fix regex used when matching variables

--- src/components/contexts/Utility.js
+++ src/components/contexts/Utility.js
@@ -25,7 +25,7 @@ export const getVariableDetails = (request) => {
    // when user used the variable as params in url i.e "{{}}" to be used in the API request
    // value will be coming from the previous response
 
-   const regexp = /{{[0-9A-Za-z]+}}/g;
+   const regexp = /{{\w+[0-9A-Za-z]+}}/g;
 
    const variablesInUrl = request.url.match(regexp) || [];
    for (const item of variablesInUrl) {
