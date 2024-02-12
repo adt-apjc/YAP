2fa0ce151768640f3057761ee7cab8c2ba0ea02f
Nuttawut <nueasaks@cisco.com>
2024-02-12 17:13:45 +0700

    bug fix when loading configuration

---
+++ frontend/src/components/Catalog/CatalogTypes.d.ts
@@ -0,0 +1,11 @@
+export type CatalogDetails = {
+   name: string;
+   version: string;
+   objective: string;
+   description: string;
+   useCases: string;
+   requirements: string;
+   labels: string[];
+   path: string;
+   iconPath: string;
+};
